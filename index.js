36ace006d5aeb7ddb22d6cf486e0f231a1f1ff22
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-12 14:33:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,47 @@ client.on('message', message => {
    message.channel.sendMessage(args.join("  "))
   }
 });
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "👑◾{ Hano Army }◾👑")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', '🌟✸🙦≼⋅welcome⋅⋟🙤✸');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "👑◾{ Hano Army }◾👑")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(`**تم دعوته من قبل ${Invite.inviter} ✨**`)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
+});
 
 client.on('message', async message => {
   if(message.content.startsWith(prefix + "setvoice")) {
