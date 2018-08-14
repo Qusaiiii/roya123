7e55f2d9a00f0999dc71b86d8aac23e8b3a0d004
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-14 19:01:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -20,31 +20,19 @@ client.on('message', message => {
     }
 });
 client.on('message', message => {
-			 if(message.content.startsWith(prefix + 'addrole')) {
-  if(message.member.hasPermission("ADMINISTRATOR")) {
-            let member2 = message.mentions.members.first();
-            if(!member2) return message.reply(":x: " + "| You need to mention a user/member!");
+			 if(message.content.startsWith(prefix + 'removerole')) {
+if(message.member.hasPermission("ADMINISTRATOR")) {
+  let member3 = message.mentions.members.first();
+  if(!member3) return message.reply(":x: " + "| You need to mention a user/member!");
 
-            let muteRole2 = message.mentions.roles.first();
-            if(!muteRole2) return message.reply(":x: " + `| There is no such role!`);
-            
-            let time2 = args[2];
-            if(!time2) {
-              member2.addRole(muteRole2.id);
-              message.channel.send(member2 + ` you have been given the permanent role: ` + muteRole2.name);
-            }else {
-              member2.addRole(muteRole2.id);
-              message.channel.send(member2 + ` you have been given the role: ` + muteRole2.name + ` for: ${ms(ms(time2), {long: true})}`);
+  let muteRole3 = message.mentions.roles.first();
+  if(!muteRole3) return message.reply(":x: " + `| There is no such thing as a \"${muteRole3.name}\" role!`);
 
-              setTimeout(function(){
-                member2.removeRole(muteRole2.id);
-                message.channel.send(member2 + ` you role has been taken off of you your glory lasted: ${ms(ms(time2), {long: true})}`)
+  member3.removeRole(muteRole3.id);
+  message.channel.send(member3 + ` you have lost the role: ` + muteRole3.name + `!`);
 
-              }, ms(time2));
-
-              };
-              }else {
-                return message.reply(":x: " + "| You need to have the \"ADMINISTRATOR\" Permission")
+  }else {
+    return message.reply(":x: " + "| You need to have the \"ADMINISTRATOR\" Permission")
 	
     }
     } 
