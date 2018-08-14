f03b6add4c5f8ef4a19caa4a0f7c1f3c0555cee0
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-14 13:10:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,45 @@ const fetchVideoInfo = require('youtube-info');
 const YouTube = require('simple-youtube-api');
 const youtube = new YouTube("AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8");
 const queue = new Map();
+client.on('message', message => {
+	   if(message.content.startsWith(prefix + 'role')) {
+    if (!msg.member.hasPermission('MANAGE_ROLES_OR_PERMISSIONS') && !msg.member.hasPermission('ADMINISTRATOR')) {
+            return msg.channel.send(':x: | You do not have the necessary permissions to perform this action!');
+        }
+        if (!msg.guild.me.hasPermission('MANAGE_ROLES_OR_PERMISSIONS')) {
+            return msg.channel.send(":x: | I can't assign or deassign roles in this server!");
+        }
 
+        var users = msg.mentions.users.array(),
+            roles = msg.content.split(' ').splice(msg.mentions.users.size).join(' ')
+                .trim()
+                .split(',');
+
+        for (var r = 0; r < roles.length; r++) {
+            let role = msg.guild.roles.find('name', roles[r]);
+
+            if (!role) {
+                msg.channel.send(':x: | Role `' + roles[r] + '` does not exist!');
+            } else if (msg.guild.members.get(bot.user.id).highestRole.comparePositionTo(role) < 1) {
+                msg.channel.send(':x: | I don\'t have permissions to edit the role `' + roles[r] + '`, please check the role order!');
+            } else if (msg.member.highestRole.comparePositionTo(role) < 1) {
+                msg.channel.send(':x: | Your highest role is lower than the role `' + roles[r] + '`, so you cannot assign it!');
+            } else {
+                for (var i = 0; i < users.length; i++) {
+                    msg.guild.members.get(users[i].id).addRole(role).then(m => {
+                        if (m.roles.has(role.id)) {
+                            msg.channel.send('Successfully added role `' + role.name + '` to ' + m.user.username + '.');
+                        } else {
+                            msg.channel.send('Failed to add role `' + role.name + '` to ' + m.user.username + '.');
+                        }
+                    })
+                        .catch(console.error);
+                }
+            }
+        }
+        return null;
+    },
+};
 client.on('message', message => {
      if (message.content === "سلام عليكم") {
       const embed = new Discord.RichEmbed()
