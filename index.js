837654f10557519b52f3ab078602998485e8bfed
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-19 13:47:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,9 @@ client.on('message', message => {
     .setColor("0x#FF0000")
 
     message.channel.send(XPEE);
-  })
+ }
+})
+});
 
 exports.fire = (text, guild) => {
     if (!guild.channels) return
