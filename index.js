92abe11ad9ccec17c95c4ce05ac91b3cae575bc6
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-19 12:33:46 +0200

    Update index.js

--- index.js
+++ index.js
@@ -42,7 +42,7 @@ client.on('message', message => {
         message.channel.sendFile(buff)
 	      })
 })
-});
+
 client.on('message', message => {
      if (message.content === "السلام عليكم") {
       const embed = new Discord.RichEmbed()
