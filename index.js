20f0d1ddff50e4fdb73c154ed34cec7a65c26057
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-19 14:05:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -52,27 +52,7 @@ client.on('message', message => {
    message.channel.sendMessage(args.join(" "))
   }
 });
- client.on('message', message =>{
-                    if(message.content.startsWith(prefix + 'xp')) {
-  let target = message.mentions.users.first() || message.guild.members.get(args[1]) || message.author;
-
-  con.query(`SELECT * FROM xp WHERE id = '${target.id}'`, (err, rows) => {
-    if(err) throw err;
-
-    if(!rows[0]) return message.channel.send("This user has no XP on record!");
-
-    let xp = rows[0].xp
-
-    var XPEE = new Discord.RichEmbed()
-    .setTitle("__***Total XP***__")
-
-    .addField("------------------------------------------",
-              "Total Ammount of XP: " + "__**" + xp + "**__" + "\n" +
-              "-----------------------------------------", true)
-
-    .setColor("0x#FF0000")
-
-    message.channel.send(XPEE);
+ 
 
 exports.fire = (text, guild) => {
     if (!guild.channels) return
