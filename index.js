c1f98aeebaa2798357adaa8d3a73f4eed5945555
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-20 14:16:27 +0200

    Update index.js

--- index.js
+++ index.js
@@ -497,10 +497,11 @@ client.on('message', message => {
                 m.send({embed: bc}).catch(err => {console.log("[Broadcast] Couldn't send message to this user because he's closing his DM!")});
             });
             message.channel.send("**:loudspeaker: | يتم إرسال البرودكسات**");
-       })
+        
+  });
+   
     }
-    
-  })
+  });
 client.on('message',function(message) {
   if(!message.channel.guild) return;
 
