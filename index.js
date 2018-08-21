02e5ec10978094806736784715348f555b0871d2
Qusaiiii <31171895+Qusaiiii@users.noreply.github.com>
2018-08-21 18:21:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -24,6 +24,65 @@ client.on('message', message => {
   message.channel.sendEmbed(embed);
     }
 });
+    client.on('message', message => {
+    if (message.content == "-امثال") {
+        var x = ["أذا ذل مولى فهو ...",
+"الإتحاد ...",
+"الناس سواسية كأسنان ...",
+"ما أشبه الليله",
+"البعد ...",
+"الماء أهون موجود وأعز ...",
+"الهزيمة تحل ...",
+"العقل ...",
+"البطنة تزيل ...",
+"اللبيب بالإشارة ...",
+"اخطب لابنتك ولا تخطب ...",
+"أعز من الولد ولد ...",
+"القرد في عين أمه ...",
+"الكتاب يقرأ من ...",
+"آخر الحياة ...",
+];
+        var x2 = ['ذليل',
+        "قوة",
+        "المشط",
+        "بالبارحة",
+        "جفاء",
+        "مفقود",
+        "العزيمة",
+        "زينة",
+        "الفطنة",
+        "يفهم",
+        "لابنك",
+        "الولد",
+        "غزال",
+        "عنوانه",
+        "الموت",
+        
+        
+        
+        
+        
+        ];
+        
+        var x3 = Math.floor(Math.random()*x.length)
+        message.channel.send(` اكمل المثل التآلي :  __**${x[x3]}**__ ؟
+    لديك 30 ثآنية للإجآبة `).then(msg1=> {
+            var r = message.channel.awaitMessages(msg => msg.content == x2[x3], {
+                maxMatches : 1,
+                time : 30000,
+                errors : ['time']
+            })
+        r.catch(() => {
+            return message.channel.send(`:negative_squared_cross_mark: لقد انتهى الوقت ولم يقم أحد بالأجابة بشكل صحيح 
+            الإجآبة الصحيحةة هي __**${x2[x3]}**__`)
+        })
+        
+        r.then((collected)=> {
+            message.channel.send(`${collected.first().author} لقد قمت بكتابة الجواب الصحيح بالوقت المناسب  `);
+        })
+        })
+    }
+})
 client.on('message',message =>{
     var prefix = "-";
     if(message.content.startsWith(prefix + 'topinv')) {
@@ -863,6 +922,7 @@ if (message.content.startsWith('-help')) { /// This is The DMS Code Send The Hel
 **-new : لفتح تذكرة جديدة **
 **-هل تعلم : Did you know?**
 **-close : لغلق التذكرة **
+**-امثال : A Game : $ **
 		 
 		 
 **اوامر ادارة السيرفر **
